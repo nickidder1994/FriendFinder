804d614383d67b2574e8b07378f2af2bde906789
Nick Kidder <nickidder@yahoo.com>
2019-08-18 13:12:17 -0700

    added html route

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,10 +1,23 @@
 var tableData = require("../data/friends");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-    app.get("/api/friends", function(req, res) {
-        res.json(friendsData);
-      });
-
-     
-    
+        app.get("/api/friends", function (req, res) {
+            res.json(friendsData);
+        });
+    }
+        app.post("/api/friends", function (req, res) {
+            if (friendsData.length < 10) {
+                friensData.push(req.body);
+                res.json(true);
+            } else {
+                console.log(incomplete)
+            }
+        });
+        app.post("/api/clear", function (req, res) {
+            // Empty out the arrays of data
+            friendsData.length = 0;
+            res.json({
+                ok: true
+            });
+        });
--- server.js
+++ server.js
@@ -5,13 +5,15 @@ var PORT = 3000;
 
 var PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 
 require("./routing/apiRoutes")(app);
 require("./routing/htmlRoutes")(app);
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
-  });
-  
+});
+
